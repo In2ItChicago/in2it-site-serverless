022e587d0fa1296542cb32db0a03dede7e521e45
Wes Thorburn <westhorburn@gmail.com>
2023-11-28 10:11:59 -0600

    Handle case when null date is submitted

--- components/Opportunity-Multi-Step-Form.vue
+++ components/Opportunity-Multi-Step-Form.vue
@@ -352,8 +352,15 @@
 	};
 
 	let submitForm = () => {
+		console.log('startDateTime', data.opportunity.startDateTime);
+		console.log('endDateTime', data.opportunity.endDateTime);
+
 		data.opportunity.startDateTime = formatDate(data.opportunity.startDateTime);
+		console.log('startDateTime formatted', data.opportunity.startDateTime);
+
 		data.opportunity.endDateTime = formatDate(data.opportunity.endDateTime);
+		console.log('endDateTime formatted', data.opportunity.startDateTime);
+		
 		emit('submit');
 	};
 
@@ -362,14 +369,17 @@
 	};
 
 	const formatDate = (date) => {
-		const year = date.getFullYear();
-		const month = String(date.getMonth() + 1).padStart(2, '0');
-		const day = String(date.getDate()).padStart(2, '0');
-		const hours = String(date.getHours()).padStart(2, '0');
-		const minutes = String(date.getMinutes()).padStart(2, '0');
+		if (date) {
+			const year = date.getFullYear();
+			const month = String(date.getMonth() + 1).padStart(2, '0');
+			const day = String(date.getDate()).padStart(2, '0');
+			const hours = String(date.getHours()).padStart(2, '0');
+			const minutes = String(date.getMinutes()).padStart(2, '0');
 
-		const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}`;
-		return formattedDate;
+			const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}`;
+			return formattedDate;
+		}
+		return '';
 	};
 
 	const thumbnails = [

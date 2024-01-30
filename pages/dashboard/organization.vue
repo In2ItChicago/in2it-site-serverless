4462b3ccd4b86255cb0bdfdee34dbcc7de4b796b
Wes Thorburn <westhorburn@gmail.com>
2024-01-30 12:58:15 -0600

    Update the account pending approval dashboard notification

--- pages/dashboard/organization.vue
+++ pages/dashboard/organization.vue
@@ -2,15 +2,28 @@
 	<div class="container-fluid">
 		<div class="row flex-nowrap">
 			<Dashboard-Sidebar></Dashboard-Sidebar>
-			<div class="col-md-4 py-3">
-				<div v-if="!organization.isApproved && !organization.isLoading">
-					<h3>
-						Account pending approval!
-					</h3>
-					<p>IN2IT reviews all organizations before their opportunities can appear on this website. Most organizations are approved <b>within 3 business days</b>. You can expect to receive an email from us soon!</p>
+			<div class="col-sm-12 col-md-8 col-xl-8 col-xxl-4 py-3">
+				<div v-if="!organization.isApproved && !organization.isLoading" class="alert alert-warning" role="alert">
+					<div class="row">
+						<div class="col-auto d-flex justify-content-center">
+							<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+								<symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+								<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+								</symbol>
+							</svg>
+							<svg class="bi" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+						</div>
+						<div class="col">
+							<b>Account pending approval</b><br>
+							
+							IN2IT reviews all organizations before their opportunities can appear on this website. Most organizations are approved within 3 business days. You can expect to receive an email from us soon!
 
-					<p>In the meantime, feel free to <a href="/dashboard/opportunities/add">add an opportunity.</a> It will automatically appear on the website as soon as your account is approved by the IN2IT team.</p>
-					<hr>
+							<br>
+							<br>
+
+							In the meantime, feel free to <a class="alert-anchor" href="/dashboard/opportunities/add">add an opportunity.</a> It will automatically appear on the website as soon as your account is approved by the IN2IT team.
+						</div>
+					</div>
 				</div>
 
 				<h1>My Organization</h1>
@@ -133,5 +146,8 @@
 </script>
 
 <style>
-	
+	.alert-anchor{
+		color:#664d03;
+		font-weight: bold;
+	}
 </style>

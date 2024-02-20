f28f00a6ae91e814f87d38ecde8686e3a7d0bd50
Wes Thorburn <westhorburn@gmail.com>
2024-02-20 09:03:06 -0600

    Fill out the for-organizations page

--- pages/for-organizations.vue
+++ pages/for-organizations.vue
@@ -5,9 +5,75 @@
 			<div class="col-md-3 col-sm-12">
 				<AboutPagesLinkGroup :page="'for-organizations'"></AboutPagesLinkGroup>
 			</div>
-			<div class="col-md-9 col-sm-12">
+			<div class="col-md-9 col-sm-12 mb-5">
 				<h1>For Organizations</h1>
+
+				<p>We help civic organizations find local volunteers seeking to make an impact in their communities. We help you connect with mission-driven volunteers, and direct them to your volunteer opportunities on whatever channels you're already using.</p>
+
+				<h2>How to Get Started</h2>
+				<ul>
+					<li>Register</li>
+					<li>Login</li>
+					<li>Fill out basic organization information</li>
+					<li>Post an event</li>
+				</ul>
+
+				<h2>Why IN2IT Chicago?</h2>
+				<ul>
+					<li>You'll be a big fish in a small pond of exclusively local organizations</li>
+					<li>Reach a mission-driven audience that is specifically seeking to make an impact locally</li>
+					<li>Further your mission alongside other impactful organizations</li>
+				</ul>
+
+				<h2>Who Can Post Events?</h2>
+				<ul>
+					<li>Mission-driven non-profit organizations that have impactful volunteer opportunities for Chicagoans.</li>
+				</ul>
+
+				<h2>Who is Not Able to Post Events?</h2>
+				<ul>
+					<li>Individuals not directly affiliated with an organization as an employee or volunteer</li>
+					<li>Partisan political organizations</li>
+					<li>Hate-groups</li>
+					<li>Anyone violating our terms</li>
+				</ul>
+
+				<h2>Priorities</h2>
+				<ul>
+					<li>Organizations with Verifiable Credentials</li>
+					<ul>
+						<li>Trustpilot</li>
+						<li>Charity Navigator</li>
+						<li>Google Reviews</li>
+					</ul>
+					<li>Organizations hosting positive, impactful, accessible volunteering experiences</li>
+					<li>Opportunities spanning the Chicago area</li>
+					<li>Local small to midsize mission driven organizations</li>
+					<li>Timely Volunteer Opportunities</li>
+					<li>Diversity of Events</li>
+					<ul>
+						<li>Times</li>
+						<li>Tasks</li>
+						<li>Issue Areas</li>
+						<li>Range of Impact</li>
+					</ul>
+				</ul>
+
+				<h2>Questions?</h2>
+				<a href="mailto:in2itchicago@gmail.com" class="email-link">Email Us!</a>
 			</div>
 		</div>
 	</div>
 </template>
+
+<style>
+	.email-link{
+		text-align: left;
+		font-family:'Nunito', sans-serif;
+		font-size:18px;
+		letter-spacing: 0px;
+		color: #034A57 !important;
+		opacity: 1;
+		text-decoration: underline;
+	}
+</style>

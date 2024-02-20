1141a02c5f7922e943fc7861c01ac0454bf0613c
Wes Thorburn <westhorburn@gmail.com>
2024-02-20 09:24:51 -0600

    Fill out for-volunteers page

--- pages/for-volunteers.vue
+++ pages/for-volunteers.vue
@@ -7,6 +7,15 @@
 			</div>
 			<div class="col-md-9 col-sm-12">
 				<h1>For Volunteers</h1>
+				<p>IN2IT Chicago is committed to featuring impactful volunteer opportunities from mission-driven Chicago organizations.</p>
+
+				<h2>Why IN2IT Chicago?</h2>
+				<ul>
+					<li>Help mission-driven organizations specifically seeking to make an impact locally</li>
+				</ul>
+				<a href="/opportunities" class="btn btn-md in2it-btn-cta">
+					Browse All Opportunities
+				</a>
 			</div>
 		</div>
 	</div>
